2f69cb522efbb833ec2c92c2f04285782615f046
TrungThanh1107 <thanh.ntr.61cntt@ntu.edu.vn>
2022-04-06 14:06:36 +0700

    Add new 2

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class CTChinh {
+public class Main {
 
 	public static void main(String[] args) throws IOException {
 		ServerSocket socketserver = new ServerSocket(7879);
