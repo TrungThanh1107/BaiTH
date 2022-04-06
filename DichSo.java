7a1a42d984b36903b0c478ced454507d74a56fc3
TrungThanh1107 <thanh.ntr.61cntt@ntu.edu.vn>
2022-04-06 14:00:01 +0700

    Add new

--- DichSo.java
+++ DichSo.java
@@ -1,45 +1,108 @@
 package baiTH;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
 
-public class DichSo extends Thread {
-	String so,ketqua;
-	Socket ClientSocket;
-	
-	int id =-1;
-	public DichSo(Socket socketClient,int id)
-	{
+public class DichSo extends Thread{
+	Socket socketClient;
+	int id = -1;
+	public DichSo(Socket socketClient, int id) {
 		super();
-		this.ClientSocket =socketClient;
-		this.id=id;
+		this.socketClient = socketClient;
+		this.id = id;
 	}
-	
-	public void run()
-	{
-		try 
-		{
-			
-			System.out.print(ClientSocket.getInetAddress().getHostAddress());
+	public void run() {
+		try {
+			System.out.print(socketClient.getInetAddress().getHostAddress());
 			System.out.print(id);
 			
-			//tao luong nhap du lieu tu ban phim
-	        DataInputStream banPhim = new DataInputStream(System.in);
-	        //tao luong nhan du lieu tu server
-	        DataInputStream inFromServer = new DataInputStream(ClientSocket.getInputStream());
-	        //tao luong gui du lieu len server
-	        DataOutputStream outToServer = new DataOutputStream(ClientSocket.getOutputStream());
-
-	        System.out.println(inFromServer.readLine());
-            System.out.println("nhap so :");
-            so = banPhim.readLine();
-            ketqua = inFromServer.readLine();
-            
-		}catch (Exception e) {
-			System.err.println(e.getMessage());
-		
+			OutputStream osToClient = socketClient.getOutputStream();
+			OutputStreamWriter write2client = new OutputStreamWriter(osToClient);
+			BufferedWriter buffWrite = new BufferedWriter(write2client);
+			
+			InputStream in = socketClient.getInputStream();
+			InputStreamReader inReader = new InputStreamReader(in);
+			BufferedReader buffRead = new BufferedReader(inReader);
+			
+			while(true){
+				String chuoiNhan=buffRead.readLine();
+				System.out.println(chuoiNhan);
+				
+				if(chuoiNhan.equals("0"))
+				{
+					String chuoiGui= "zero";
+					buffWrite.write(chuoiGui+"\n");
+					buffWrite.flush();
+				}
+				if(chuoiNhan.equals("1")) 
+				{
+					String chuoiGui= "one";
+					buffWrite.write(chuoiGui+"\n");
+					buffWrite.flush();
+				} 
+				if(chuoiNhan.equals("2")) 
+				{
+					String chuoiGui= "two";
+					buffWrite.write(chuoiGui+"\n");
+					buffWrite.flush();
+				}
+				if(chuoiNhan.equals("3")) 
+				{
+					String chuoiGui= "three";
+					buffWrite.write(chuoiGui+"\n");
+					buffWrite.flush();
+				}
+				if(chuoiNhan.equals("4")) 
+				{
+					String chuoiGui= "four";
+					buffWrite.write(chuoiGui+"\n");
+					buffWrite.flush();
+				}
+				if(chuoiNhan.equals("5")) 
+				{
+					String chuoiGui= "five";
+					buffWrite.write(chuoiGui+"\n");
+					buffWrite.flush();
+				}
+				if(chuoiNhan.equals("6")) 
+				{
+					String chuoiGui= "six";
+					buffWrite.write(chuoiGui+"\n");
+					buffWrite.flush();
+				}
+				if(chuoiNhan.equals("7")) 
+				{
+					String chuoiGui= "seven";
+					buffWrite.write(chuoiGui+"\n");
+					buffWrite.flush();
+				}
+				if(chuoiNhan.equals("8")) 
+				{
+					String chuoiGui= "eight";
+					buffWrite.write(chuoiGui+"\n");
+					buffWrite.flush();
+				}
+				if(chuoiNhan.equals("9")) 
+				{
+					String chuoiGui= "nine";
+					buffWrite.write(chuoiGui+"\n");
+					buffWrite.flush();
+				}
+				if(chuoiNhan.equals("10")) 
+				{
+					break;
+				}
+				
+			}
+			socketClient.close();
+			
+		}catch(Exception e) {
+			System.out.print(e.getMessage());
 		}
 	}
-	
 }
